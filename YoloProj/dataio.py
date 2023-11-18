eb25b72d7019d20e6a8496705656fc1a043681ca
vanillaPenguin <kh0204@kakao.com>
2023-11-18 18:32:13 +0900

    minor

--- YoloProj/dataio.py
+++ YoloProj/dataio.py
@@ -2,7 +2,7 @@ import os
 import torch
 from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as T
-from videoio import *
+from videoio import * # => resize_img resize_video 는 Tensor를 반환함
 import yaml
 
 def get_labels() -> list[tuple[str, float, float, float, float]]:
